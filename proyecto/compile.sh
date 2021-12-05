0eb60114d60d0d3b4c678ce2b151fa42a828a550
Guillermo Daniel Toyos Marfurt <guillermo.toyos@fing.edu.uy>
2021-12-05 12:58:40 -0300

    ex

--- proyecto/compile.sh
+++ proyecto/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 
-export JAVA_HOME="~/lejava"
+export JAVA_HOME=~/lejava
 ~/lemaven/bin/mvn clean install
 cp target/AEProject-0.0.1-SNAPSHOT.jar ae.jar
---
+++ proyecto/exe.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+
+~/lejava/bin/java -jar -Xms40G ae.jar 100000 35 exp
