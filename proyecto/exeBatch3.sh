10cd92623753fd08ea2d8ce0976c9a20d501acab
Guillermo Daniel Toyos Marfurt <guillermo.toyos@fing.edu.uy>
2021-12-06 10:48:29 -0300

    lol

--- proyecto/exeBatch3.sh
+++ proyecto/exeBatch3.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=aeInstancia3
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=39
-#SBATCH --mem=70G
+#SBATCH --mem=100G
 #SBATCH --time=48:00:00
 #SBATCH --partition=besteffort
 #SBATCH --qos=besteffort
@@ -14,4 +14,4 @@ source /etc/profile.d/modules.sh
 export JAVA_HOME=~/lejava
 
 cd ~/ae2021_practico/proyecto
-~/lejava/bin/java -jar -Xms69G ae.jar 1000 16 exp 3
+~/lejava/bin/java -jar -Xms69G -Xmx100G ae.jar 1000 39 exp 3
